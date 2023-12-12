738c3022a85e76ff60eb8e40cdbe6d8d8009fc47
Alessio Brioschi <77493369+Brioche23@users.noreply.github.com>
2023-12-12 12:04:42 +0100

    First Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: ['GT Maru Mega'],
+        body: ['GT Maru Shaded'],
+      },
+    },
   },
   plugins: [],
   darkMode: ['class', '.darkmode'],
